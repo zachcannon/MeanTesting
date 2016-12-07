6e7586bd2d602ae46d7d8644ab547ade91815069
zachcannon <zach.cannon10@gmail.com>
2016-12-06 21:08:41 -0600

    Added in node code from angular class, refactored server.js, renamed client to app

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 var path = require('path');
+var rootPath = path.normalize(__dirname + '/../');
 var express = require('express');
+var events = require('./eventsController');
 
 var app = express();
-
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(rootPath + '/app'));
 
 app.get('/',function(req, res) {
-    res.sendFile(__dirname + '../client/index.html');
+    res.sendFile(rootPath + '/app/index.html');
     res.send();
 });
+app.get('/data/event/:id', events.get);
+app.get('/data/event', events.getAll);
+app.post('/data/event/:id', events.save);
+app.get('*', function(req, res) { res.sendFile(rootPath + '/app/index.html'); });
+
 app.listen(process.env.PORT, 'localhost', (np) => {
   console.log(`LOGGING: Server running at http://localhost:${process.env.PORT}/`);
 });
